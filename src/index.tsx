181ede1e8ad39e0c8c5a7fc4e32cfbba2d01126c
zterhes <zterhes1@gmail.com>
2022-02-12 14:26:26 +0100

    rename App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DashboardPage from './DashboardPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DashboardPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
